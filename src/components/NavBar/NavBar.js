036f99d3aa8d622a7158d8a6a8aa2c73004cc805
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-10-08 12:52:22 +0300

    curatat clase nefolosite

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ function NavBar()
 {
     return (
       
-<nav className="border-gray-200 bg-slate-100 px-2 sm:px-4 py-2.5 rounded">
+<nav className=" bg-slate-100 px-2 sm:px-4 py-2.5 rounded">
   <div className="container flex flex-wrap justify-between items-center mx-auto">
     <a href="/" className="flex items-center">
         <img src={logo_symbol} className="mr-3 h-6 sm:h-9" alt="Main Logo" />
@@ -20,7 +20,7 @@ function NavBar()
         <img src={titlu} className="mr-3 h-6 sm:h-9" alt="Title" />
     </a>
     <div className=" w-full md:block md:w-auto" id="navbar-default">
-      <ul className="flex flex-col p-4 mt-4 bg-slate-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 border-gray-700">
+      <ul className="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 border-gray-700">
         <li>
           <a href="/" className="block py-2 pr-4 pl-3 my-0 text-gray-700 hover:text-ana-purple md:border-0 md:p-0 md:my-4">
             Acasă</a>
@@ -42,22 +42,22 @@ function NavBar()
             Contact</a>
         </li>
         <li>
-          <a href="/" className="block p-0 m-0 md:border-0 md:p-0 text-gray-700 hover:text-ana-purple ">
+          <a href="/" className="block p-0 m-0 md:border-0 md:p-0">
             <img src={fb} alt="Facebook logo"/>
           </a>
         </li>
         <li>
-          <a href="/" className="block p-0 m-0 md:border-0 md:p-0 text-gray-700 hover:text-ana-purple ">
+          <a href="/" className="block p-0 m-0 md:border-0 md:p-0">
             <img src={insta} alt="Instagram logo"/>
           </a>
         </li>
         <li>
-          <a href="/" className="block p-0 m-0 md:border-0 md:p-0 text-gray-700 hover:text-ana-purple ">
+          <a href="/" className="block p-0 m-0 md:border-0 md:p-0">
             <img src={wapp} alt="Whatsapp logo"/>
           </a>
         </li>
         <li>
-          <a href="/" className="block p-0 m-0 md:border-0 md:p-0 text-gray-700 hover:text-ana-purple ">
+          <a href="/" className="block p-0 m-0 md:border-0 md:p-0">
             <img src={mail} alt="Mail logo"/>
           </a>
         </li>
