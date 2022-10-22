be9121f963d69e1d0f8f980b11bb86877cdb2720
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-10-22 12:27:00 +0300

    updated navbar logos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,8 +3,8 @@ import nume from './media/Ana-Maria-Sfirca.png';
 import titlu from './media/Psihoterapeut-integrativ.png';
 import psihozone from './media/psihozone.png';
 
-import logo_ana from './media/logo_ana.png';
-import logo_psihozone from './media/logo_psihozone.png';
+import logo_ana from './media/AS.png';
+import logo_psihozone from './media/PZ.png';
 
 import meniu_mobil_mov from './media/meniu_mobil_mov.png';
 import meniu_mobil_albastru from './media/meniu_mobil_albastru.png';
@@ -37,7 +37,7 @@ export default function NavBar()
 <nav className="bg-slate-100">
       <ul className="md:flex md:flex-row p-3 items-center justify-center">
         <li className="flex justify-center ml-10 md:m-0">
-          <a href="/" className="flex flex-row items-center">
+          <a href="/" className="flex flex-row items-center space-x-2">
             <img src={(current_url === "/psihozone" ? logo_psihozone: logo_ana)} className="w-10" alt="Main Logo"/>
             <div className="flex flex-col">
               <img src={(current_url === "/psihozone" ? psihozone: nume)} className="w-5/6 md:w-3/4" alt="Name"/>
@@ -50,15 +50,15 @@ export default function NavBar()
           </button>
         </li>
         <li className={"md:flex flex-col md:flex-row md:space-x-8 md:pr-16 md:mx-24 mb-6 md:mb-0 mt-2 " + (state ? "hidden":"")}>
-          <a href="/" className={"flex items-center justify-center text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple':'')}>
+          <a href="/" className={"flex items-center justify-center hover:overline text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple':'')}>
               Acasă </a>
-          <a href="/despre_mine" className={"flex items-center justify-center text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple':'')}>
+          <a href="/despre_mine" className={"flex items-center justify-center hover:overline text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple':'')}>
             Despre mine </a>
-          <a href="/servicii" className={"flex items-center justify-center text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple':'')}>
+          <a href="/servicii" className={"flex items-center justify-center hover:overline text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple':'')}>
             Servicii </a>
-          <a href="/psihozone" className={"flex items-center justify-center text-lg text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue':'')}>
+          <a href="/psihozone" className={"flex items-center justify-center hover:overline text-lg text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue':'')}>
             Psihozone </a>
-          <a href="/contact" className={"flex items-center justify-center text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple':'')}>
+          <a href="/contact" className={"flex items-center justify-center hover:overline text-lg text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple':'')}>
             Contact </a>
         </li>
         <li className={"md:flex flex-row items-center justify-center md:space-x-2 " + (state ? "hidden":"flex")}>
