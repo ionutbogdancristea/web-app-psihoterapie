b2a60f03c4dc302369277a49659a7cc86f25eef7
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-10-22 12:53:24 +0300

    added new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,20 @@ module.exports = {
         'ana-blue':'#2596be',
       },
       fontFamily: {
-        museoslab500:["museo-slab-500"],
         museoslab100:["museo-slab-100"],
+        museoslab100italic:["museo-slab-100-italic"],
+        museoslab300:["museo-slab-300"],
+        museoslab300italic:["museo-slab-300-italic"],
+        museoslab500:["museo-slab-500"],
+        museoslab500italic:["museo-slab-500-italic"],
+        museoslab700:["museo-slab-700"],
+        museoslab700italic:["museo-slab-700-italic"],
+        museoslab900:["museo-slab-900"],
+        museoslab900italic:["museo-slab-900-italic"],
+        museoslab1000:["museo-slab-1000"],
+        museoslab1000italic:["museo-slab-1000-italic"],
+        sourceserifprolight:["source-serif-pro-light"],
+        sourceserifvariableroman:["source-serif-variable-roman"]
       }
     },
   },
