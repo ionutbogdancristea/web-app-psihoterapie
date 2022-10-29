5514abb320a6c95322dcb1812d7c698276c7a1ce
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-10-29 14:45:43 +0300

    work on acasa page

--- src/pages/Acasa/Acasa.js
+++ src/pages/Acasa/Acasa.js
@@ -1,17 +1,20 @@
-import munte from './media/munte.jpg'
+import acasa from './media/acasa.jpeg'
+import line from './media/white_line.png'
 
 function AcasaPage() 
 {
     return <div>
-        <div className="m-6 flex flex-row bg-cover bg-[url('/src/pages/Acasa/media/chenar.png')]"> 
-            <div className="w-2/4 text-center m-auto md:p-14 p-2">
-                <div className="md:text-3xl text-left">Psihoterapie individuală și de grup</div>
-                <div className="text-left"> -------- </div>
-                <div className="text-left">Cucerește-ți propria viață!</div>
+        <div className="bg-[url('/src/pages/Acasa/media/chenar.png')] bg-no-repeat">
+            <div>&nbsp;</div>
+            <div className="flex flex-row items-center justify-center space-x-24"> 
+                <div className="text-center">
+                        <div className="text-left md:text-3xl font-museoslab300 text-white">Psihoterapie individuală și de grup</div>
+                        <img src={line} alt="linie"></img>
+                        <div className="text-left md:text-2xl font-museoslab300 text-white">Cucerește-ți propria viață!</div>
+                </div>
+                <img className="rounded-lg object-contain max-w-md" src={acasa} alt="view"></img>
             </div>
-            <div className="w-2/4">
-                <img className="rounded-lg" src={munte} alt="view"></img>
-            </div>
+            <div>&nbsp;</div>
         </div>
 </div>;
 }
