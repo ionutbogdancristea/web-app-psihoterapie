b7cd078ca0fe19ba42317ea9cd8f98bd59b8d4f6
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-11-03 16:51:21 +0200

    removed manifest trace + resize Acasa

--- src/pages/Acasa/Acasa.js
+++ src/pages/Acasa/Acasa.js
@@ -9,7 +9,7 @@ function AcasaPage()
             <div className="flex flex-row items-center justify-center space-x-24"> 
                 <div className="text-center">
                         <div className="text-left md:text-3xl font-museoslab300 text-white">Psihoterapie individuală și de grup</div>
-                        <img src={line} alt="linie"></img>
+                        <img src={line} alt="linie" className="w-16"/>
                         <div className="text-left md:text-2xl font-museoslab300 text-white">Cucerește-ți propria viață!</div>
                 </div>
                 <img className="rounded-lg object-contain max-w-md" src={acasa} alt="view"></img>
