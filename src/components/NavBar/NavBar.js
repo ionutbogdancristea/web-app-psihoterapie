623fde752c719fc124d336a4118219ec409f2c36
ionutbogdancristea <ionutbogdancristea@gmail.com>
2022-11-05 16:55:11 +0200

    deployed on github pages

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import { useLocation } from 'react-router-dom';
 
 import nume from './media/Ana-Maria-Sfirca.png';
 import titlu from './media/Psihoterapeut-integrativ.png';
@@ -29,7 +30,8 @@ import mail_gri from './media/mail_gri.png';
 export default function NavBar() 
 {
   // variable used to color the current page's name in pink:
-  let current_url = document.location.pathname;
+  let current_url = useLocation().pathname;
+  console.log(current_url)
 
   // objects used in the Menu button for mobile
   const [state, setState] = useState(true);
@@ -42,7 +44,7 @@ export default function NavBar()
 <nav className="bg-slate-100">
       <ul className="md:flex md:flex-row p-3 items-center justify-center">
         <li className="flex justify-center ml-10 md:m-0">
-          <a href="/" className="flex flex-row items-center space-x-2">
+          <a href="#/" className="flex flex-row items-center space-x-2">
             <img src={(current_url === "/psihozone" ? logo_psihozone: logo_ana)} className="w-10" alt="Main Logo"/>
             <div className="flex flex-col">
               <img src={(current_url === "/psihozone" ? psihozone: nume)} className="w-5/6 md:w-3/4" alt="Name"/>
@@ -55,27 +57,27 @@ export default function NavBar()
           </button>
         </li>
         <li className={"md:flex flex-col md:flex-row md:space-x-8 md:pr-16 md:mx-24 mb-6 md:mb-0 mt-2 " + (state ? "hidden":"")}>
-          <a href="/" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple':'')}>
+          <a href="#/" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple':'')}>
               Acasa </a>
-          <a href="/despre_mine" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple':'')}>
+          <a href="#/despre_mine" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple':'')}>
             Despre mine </a>
-          <a href="/servicii" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple':'')}>
+          <a href="#/servicii" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple':'')}>
             Servicii </a>
-          <a href="/psihozone" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue':'')}>
+          <a href="#/psihozone" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue':'')}>
             Psihozone </a>
-          <a href="/contact" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple':'')}>
+          <a href="#/contact" className={"flex items-center justify-center hover:overline text-lg font-museoslab500 text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple':'')}>
             Contact </a>
         </li>
         <li className={"md:flex flex-row items-center justify-center md:space-x-2 " + (state ? "hidden":"flex")}>
           <a href="https://www.facebook.com/anamariasfirca.psiholog/" className="" target="_blank" rel="noopener noreferrer">
             <img src={(current_url === "/psihozone" ? facebook_albastru : facebook_mov)} onMouseOver={e => (e.currentTarget.src = facebook_gri)} 
-            onMouseOut={e => (current_url === "/psihozone" ? e.currentTarget.src = facebook_albastru : e.currentTarget.src = facebook_mov)} className="w-3/4" alt="Facebook logo"/>
+            onMouseOut={e => (current_url === "#/psihozone" ? e.currentTarget.src = facebook_albastru : e.currentTarget.src = facebook_mov)} className="w-3/4" alt="Facebook logo"/>
           </a>
           <a href="https://www.instagram.com/anasfirca_psihozone/" className="" target="_blank" rel="noopener noreferrer">
             <img src={(current_url === "/psihozone" ? instagram_albastru : instagram_mov)} onMouseOver={e => (e.currentTarget.src = instagram_gri)} 
-            onMouseOut={e => (current_url === "/psihozone" ? e.currentTarget.src = instagram_albastru : e.currentTarget.src = instagram_mov)} className="w-3/4" alt="Instagram logo"/>
+            onMouseOut={e => (current_url === "#/psihozone" ? e.currentTarget.src = instagram_albastru : e.currentTarget.src = instagram_mov)} className="w-3/4" alt="Instagram logo"/>
           </a>
-          <a href="/" className="">
+          <a href="#/" className="">
             <img src={(current_url === "/psihozone" ? whatsapp_albastru : whatsapp_mov)} onMouseOver={e => (e.currentTarget.src = whatsapp_gri)} 
             onMouseOut={e => (current_url === "/psihozone" ? e.currentTarget.src = whatsapp_albastru : e.currentTarget.src = whatsapp_mov)} className="w-3/4" alt="Whatsapp logo"/>
           </a>
