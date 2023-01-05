0d1b825b8132f3af7ca502d8a1ce292820193270
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-01-05 18:21:57 +0200

    rearanged contact footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ import mail from './media/mail.png'
 function Contact() 
 {
     return <div className="bg-ana-light-purple relative bottom-0 flex flex-col"> 
-    <div className="font-sourceserifvariableroman md:text-xl text-white text-center">Contact</div>
+    <div className="font-sourceserifvariableroman md:text-xl text-white text-center mt-1">Contact</div>
     <div className="flex flex-row text-center pl-[18%] md:pl-[44%]">
         <img className="w-4 mr-2" src={telefon} alt="telefon"/>
         <div className="font-sourceserifvariableroman">0771611430</div>
@@ -19,7 +19,7 @@ function Contact()
         <img className="w-4 mr-2" src={locatie} alt="locatie"/>
         <div className="font-sourceserifvariableroman">Chiajna, Str. Serg. Ilie Petre 8C</div>
     </div>
-    <div className="flex flex-row items-center pl-[18%] md:pl-[44%]">
+    <div className="flex flex-row items-center pl-[18%] md:pl-[44%] mb-2">
         <img className="w-4 mr-2" src={mail} alt="mail"/>
         <div className="font-sourceserifvariableroman">anamariasfirca.psiholog@gmail.com</div>
     </div>
