60eaf6bf926a5b1c93d3a7af08236ca7ac6b3d2c
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-01-05 20:03:00 +0200

    draft on Despre mine

--- src/pages/DespreMine/DespreMine.js
+++ src/pages/DespreMine/DespreMine.js
@@ -1,16 +1,16 @@
-import ana from './Media/ana.png'
+import ana from './media/ana.png'
+import { text1 } from './DespreMineData';
 
 function DespreMinePage() 
 {
-    return <div>
+    return <div className="bg-[url('/src/pages/DespreMine/media/fundal-servicii_0.png')] bg-no-repeat">
         <div className="flex flex-row">
-        <div className="w-4/5 pl-20">
-            Bee Movie is a 2007 American computer-animated comedy film produced by DreamWorks Animation and distributed by Paramount Pictures. Directed by Simon J. Smith and Steve Hickner from a screenplay by Jerry Seinfeld, Spike Feresten, Barry Marder and Andy Robin, it stars the voices of Seinfeld, Renée Zellweger, Matthew Broderick, John Goodman, Patrick Warburton, and Chris Rock. The film centers on Barry B. Benson (Seinfeld), a honey bee who tries to sue the human race for exploiting bees after learning from his florist friend Vanessa Bloome (Zellweger) that humans sell and consume honey.Bee Movie debuted in New York City on October 25, 2007, and was released in theaters in the United States on November 2. Upon its release, it received mixed reviews from critics, who praised its humor and voice cast, but criticized its plot. Nevertheless, the film was a moderate box office success, grossing $293 million worldwide. It has since gathered a cult following, partly driven by memes of the film shared on social media.
-        </div>
-        <div style={{alignItems: 'center',justifyContent: 'center',}}>
-            <img src={ana} alt="Ana" className="max-h-96"></img>
-        </div>
-
+            <div className="w-4/5 pl-20">
+                {text1}
+            </div>
+            <div style={{alignItems: 'center',justifyContent: 'center',}}>
+                <img src={ana} alt="Ana" className="max-h-96"></img>
+            </div>
         </div> 
 
     </div>;
