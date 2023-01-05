fa3cff685a5a580b5ce4f5f28335c7cc99f15f3e
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-01-05 18:44:20 +0200

    final draft servicii

--- src/components/ServiciiElement/ServiciiElement.js
+++ src/components/ServiciiElement/ServiciiElement.js
@@ -1,4 +1,4 @@
-import linie from './media/linie-la-card-articole.png'
+import linie from './media/linie-chenar.png'
 
 function ServiciiElement({items}) 
 {
@@ -8,15 +8,15 @@ function ServiciiElement({items})
             <div key={index}> 
               <div className="bg-[url('/src/components/ServiciiElement/media/chenar-servicii.png')] bg-no-repeat">
                 <div className={(index % 2 === 0 ? "md:flex md:justify-between " : "md:flex md:flex-row-reverse md:justify-between ")+ "md:space-x-6 p-6"}>
-                    <img src={item.image} alt={item.alt} className="h-48"></img>
+                    <img src={item.image} alt={item.alt} className="h-56"></img>
                     <div className="flex flex-col">
-                        <div className='text-center'>
-                            <div className="text-xl text-ana-purple">
+                        <div className='text-center mb-3'>
+                            <div className="text-2xl text-ana-purple font-sourceserifvariableroman">
                                 {item.title}
                             </div>
                             <img src={linie} alt="linie max-w-xs" className="mx-auto"/>
                         </div>
-                        <div>
+                        <div className="font-sourceserifvariableroman text-base">
                             {item.text}
                         </div>
                     </div>
