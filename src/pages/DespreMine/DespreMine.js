8bb58c9c2d90c3a73b7662ed72395298d9b54439
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-01-07 11:50:37 +0200

    corrected and added all text DespreMine page

--- src/pages/DespreMine/DespreMine.js
+++ src/pages/DespreMine/DespreMine.js
@@ -1,12 +1,18 @@
 import ana from './media/ana.png'
-import { text1 } from './DespreMineData';
+import { text1, text2, text3, text4 } from './DespreMineData';
 
 function DespreMinePage() 
 {
     return <div className="bg-[url('/src/pages/DespreMine/media/fundal-servicii_0.png')] bg-no-repeat">
         <div className="flex flex-row">
             <div className="w-4/5 pl-20">
+                Sa ne cunoastem!
                 {text1}
+                {text2}
+                Viziunea mea
+                {text3}
+                Misiunea mea
+                {text4}
             </div>
             <div style={{alignItems: 'center',justifyContent: 'center',}}>
                 <img src={ana} alt="Ana" className="max-h-96"></img>
