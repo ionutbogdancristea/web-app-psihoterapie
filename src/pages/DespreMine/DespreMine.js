98e5bf1384c9f10050f1a677c75ef1fab13d5ee6
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-01-07 13:32:06 +0200

    final draft despre mine page

--- src/pages/DespreMine/DespreMine.js
+++ src/pages/DespreMine/DespreMine.js
@@ -1,25 +1,47 @@
+import Contact from '../../components/Contact/Contact';
 import ana from './media/ana.png'
 import { text1, text2, text3, text4 } from './DespreMineData';
+import linie from '../../media/linie-chenar-jum.png';
 
 function DespreMinePage() 
 {
     return <div className="bg-[url('/src/pages/DespreMine/media/fundal-servicii_0.png')] bg-no-repeat">
-        <div className="flex flex-row">
-            <div className="w-4/5 pl-20">
-                Sa ne cunoastem!
-                {text1}
-                {text2}
-                Viziunea mea
-                {text3}
-                Misiunea mea
-                {text4}
-            </div>
-            <div style={{alignItems: 'center',justifyContent: 'center',}}>
-                <img src={ana} alt="Ana" className="max-h-96"></img>
+        <div className="p-14">
+        <div className="flex md:flex-row-reverse flex-col">
+            <img src={ana} alt="profil" className="md:h-64 md:ml-6 mb-14"></img>
+            <div>
+                <div className="text-2xl md:text-3xl text-ana-purple font-sourceserifvariableroman">
+                    Să ne cunoaștem!
+                </div>
+                <img src={linie} alt="linie" className="pb-4"/>
+                <div className="ml-4 font-sourceserifvariableroman text-lg">
+                    {text1}
+                </div>
+                <div>&nbsp;</div>
+                <div className="ml-4 font-sourceserifvariableroman text-lg">
+                    {text2}
+                </div>
             </div>
         </div> 
 
+            <div className="text-2xl md:text-3xl text-ana-purple font-sourceserifvariableroman mt-10">
+                Viziunea mea
+            </div>
+            <img src={linie} alt="linie" className="pb-4"/>
+            <div className="ml-4 mr-8 font-sourceserifvariableroman text-lg">
+                {text3}
+            </div>
+
+            <div className="text-2xl md:text-3xl text-ana-purple font-sourceserifvariableroman mt-12 md:mt-16">
+                Misiunea mea
+            </div>
+            <img src={linie} alt="linie" className="pb-4"/>
+            <div className="ml-4 mr-8 font-sourceserifvariableroman text-lg pb-12">
+                {text4}
+            </div>
+        </div>
+            <Contact/>
     </div>;
-}
+}   
 
 export default DespreMinePage;
