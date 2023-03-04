ba23217481ba16b2b735c3825c8e7864da2ad40c
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-03-04 14:56:46 +0200

    navbar menu visual changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -56,19 +56,19 @@ export default function NavBar()
             <img src={(current_url === "/psihozone" ? meniu_mobil_albastru : meniu_mobil_mov)} className="w-10" alt="Menu Icon"/>
           </button>
         </li>
-        <li className={"md:flex flex-col md:flex-row md:space-x-8 md:pr-16 md:mx-24 mb-6 md:mb-0 mt-2 noBlueHighlight " + (state ? "hidden":"")}>
-          <a href="#/" className={"flex items-center justify-center decoration-wavy text-xl font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple':'')}>
-              Acasă </a>
-          <a href="#/despre_mine" className={"flex items-center justify-center decoration-wavy text-xl font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple':'')}>
-            Despre mine </a>
-          <a href="#/servicii" className={"flex items-center justify-center decoration-wavy text-xl font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple':'')}>
-            Servicii </a>
-          <a href="#/psihozone" className={"flex items-center justify-center decoration-wavy text-xl font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue':'')}>
-            PsihoZone </a>
-          <a href="#/contact" className={"flex items-center justify-center decoration-wavy text-xl font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple':'')}>
-            Contact </a>
+        <li className={"md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 md:pr-16 md:mx-14 mb-6 md:mb-0 mt-2 noBlueHighlight " + (state ? "hidden":"")}>
+          <a href="#/" className={"p-1 md:p-0 flex justify-center text-sm font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/" ? 'text-ana-purple border md:border-0':'')}>
+              ACASĂ </a>
+          <a href="#/despre_mine" className={"p-1 md:p-0 flex justify-center decoration-wavy text-sm font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/despre_mine" ? 'text-ana-purple border md:border-0':'')}>
+            DESPRE MINE </a>
+          <a href="#/servicii" className={"p-1 md:p-0 flex justify-center decoration-wavy text-sm font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/servicii" ? 'text-ana-purple border md:border-0':'')}>
+            SERVICII </a>
+          <a href="#/psihozone" className={"p-1 md:p-0 flex justify-center decoration-wavy text-sm font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-blue active:text-blue-700 " + (current_url === "/psihozone" ? 'text-ana-blue border md:border-0':'')}>
+            PSIHOZONE </a>
+          <a href="#/contact" className={"p-1 md:p-0 flex justify-center decoration-wavy text-sm font-medium font-sourceserifvariableroman text-gray-600 hover:text-ana-purple active:text-violet-700 " + (current_url === "/contact" ? 'text-ana-purple border md:border-0':'')}>
+            CONTACT </a>
         </li>
-        <li className={"md:flex flex-row items-center justify-center md:space-x-2 " + (state ? "hidden":"flex")}>
+        <li className={"md:flex flex-row items-center justify-center space-x-8 md:space-x-4 " + (state ? "hidden":"flex")}>
           <a href="https://www.facebook.com/anamariasfirca.psiholog/" className="" target="_blank" rel="noopener noreferrer">
             <img src={(current_url === "/psihozone" ? facebook_albastru : facebook_mov)} onMouseOver={e => (e.currentTarget.src = facebook_gri)} 
             onMouseOut={e => (current_url === "/psihozone" ? e.currentTarget.src = facebook_albastru : e.currentTarget.src = facebook_mov)} className="w-3/4" alt="Facebook logo"/>
