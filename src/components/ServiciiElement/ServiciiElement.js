9e66018dfa62cf74f1d7f173d93e90cf0e6f645c
ionutbogdancristea <ionutbogdancristea@gmail.com>
2023-03-21 19:53:10 +0200

    servicii page - corrected space between image and text

--- src/components/ServiciiElement/ServiciiElement.js
+++ src/components/ServiciiElement/ServiciiElement.js
@@ -7,9 +7,9 @@ function ServiciiElement({items})
         return  (
             <div key={index}> 
               <div className="bg-ana-purple-servicii bg-opacity-20">
-                <div className={(index % 2 === 0 ? "md:flex md:justify-between " : "md:flex md:flex-row-reverse md:justify-between ")+ "md:space-x-8 py-6 px-6 md:px-80 items-center"}>
+                <div className={(index % 2 === 0 ? "md:flex md:justify-between " : "md:flex md:flex-row-reverse md:justify-between ")+ "py-6 px-6 md:px-80 items-center"}>
                     <img src={item.image} alt={item.alt} className="h-56"></img>
-                    <div className="flex flex-col">
+                    <div className={(index % 2 === 0 ? "md:ml-5" : "md:mr-5") + " flex flex-col md:mr-6"}>
                         <div className='text-center mb-3 mt-3 md:mt-0'>
                             <div className="text-2xl text-ana-purple">
                                 {item.title}
